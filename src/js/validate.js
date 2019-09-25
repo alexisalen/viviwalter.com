f0e9357680f416afcb5046884af1212e1ba76db3
Alexis Walter <alexis.alen@gmail.com>
2019-09-25 17:21:43 +0200

    Form validator submit issue fix

--- src/js/validate.js
+++ src/js/validate.js
@@ -2,7 +2,7 @@
 
 var form = document.querySelector("#formulario");
 
-form.addEventListener('submit', function(){
+form.addEventListener('submit', function(event){
 
     var nombre = document.querySelector("#nombre").value;
     var nom_style = document.querySelector("#nombre");
@@ -16,6 +16,7 @@ form.addEventListener('submit', function(){
     if (nombre.trim() <= 0  || nombre.trim() == null || !isNaN(nombre)){
         document.querySelector("#error").innerHTML = "Error encontrado! Verifique los campos.";
         nom_style.style.border = "1px solid #cd0000";
+        event.preventDefault();
 
     } else {
         document.querySelector("#error_nom").style.display = "none";
@@ -25,6 +26,7 @@ form.addEventListener('submit', function(){
     if (email.trim() <= 0  || email.trim() == null){
         document.querySelector("#error").innerHTML = "Error encontrado! Verifique los campos.";
         ema_style.style.border = "1px solid #cd0000";
+        event.preventDefault();
 
     } else {
         document.querySelector("#error_nom").style.display = "none";
@@ -34,6 +36,7 @@ form.addEventListener('submit', function(){
     if (asunto.trim() <= 0  || asunto.trim() == null || asunto.length > 80) {
         document.querySelector("#error").innerHTML = "Error encontrado! Verifique los campos.";
         sub_style.style.border = "1px solid #cd0000";
+        event.preventDefault();
 
     } else {
         document.querySelector("#error_nom").style.display = "none";
@@ -41,5 +44,3 @@ form.addEventListener('submit', function(){
     }
     
 });
-
-
